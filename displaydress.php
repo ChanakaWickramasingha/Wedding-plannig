2538620420d51662990e80695d9e00678905afbd
Amantha Chanaka <amanthachanaka223@gmail.com>
2024-07-04 14:42:53 +0530

    change the elements of dress page from css file

--- displaydress.php
+++ displaydress.php
@@ -1,12 +1,23 @@
 <?php
 require "config.php";
-require "header.php";
-
 
 $sql = "SELECT name,address,dress,image,description,contact FROM wedding_dress";
 
 $result = $con->query($sql);
+?>
 
+<?php
+require "header.php";
+?>
+<!DOCTYPE html>
+<head>
+    <link rel = "stylesheet" href = "style/displaydress.css">
+</head>
+<body>
+    <h2>Wedding dresses</h2>
+    <div class ="dress-container">
+
+<?php
 if($result -> num_rows > 0 ){
     while($row = $result -> fetch_assoc() ){
         echo '<div class="dress-item">';
@@ -25,12 +36,11 @@ else{
 }
 
 $con->close();
+?>
+</div>
+</body>
+</html>
 
+<?php
 require "footer.php";
 ?>
-<!DOCTYPE html>
-<head>
-    <link rel = "stylesheet" href = "style/displaydress.css">
-</head>
-<body></body>
-</html>
